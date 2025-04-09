b99518f5016ed785058d50afbbd7e2aba152d7c7
BenjaDuran23 <bfduran@miuandes.cl>
2025-04-09 18:39:24 -0400

    Adding class for chat, message and user

---
+++ db/seeds.rb
@@ -0,0 +1,41 @@
+# This file should ensure the existence of records required to run the application in every environment (production,
+# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Example:
+#
+#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
+#     MovieGenre.find_or_create_by!(name: genre_name)
+#   end
+User.Create(email: ,first_name: ,last_name: )
+User.Create(email: ,first_name: ,last_name: )
+User.Create(email: ,first_name: ,last_name: )
+User.Create(email: ,first_name: ,last_name: )
+User.Create(email: ,first_name: ,last_name: )
+User.Create(email: ,first_name: ,last_name: )
+User.Create(email: ,first_name: ,last_name: )
+User.Create(email: ,first_name: ,last_name: )
+User.Create(email: ,first_name: ,last_name: )
+User.Create(email: ,first_name: ,last_name: )
+
+Chat.Create(sender_id: ,receiver_id: )
+Chat.Create(sender_id: ,receiver_id: )
+Chat.Create(sender_id: ,receiver_id: )
+Chat.Create(sender_id: ,receiver_id: )
+Chat.Create(sender_id: ,receiver_id: )
+Chat.Create(sender_id: ,receiver_id: )
+Chat.Create(sender_id: ,receiver_id: )
+Chat.Create(sender_id: ,receiver_id: )
+Chat.Create(sender_id: ,receiver_id: )
+Chat.Create(sender_id: ,receiver_id: )
+
+Message.Create(chat_id: ,user_id: ,body: )
+Message.Create(chat_id: ,user_id: ,body: )
+Message.Create(chat_id: ,user_id: ,body: )
+Message.Create(chat_id: ,user_id: ,body: )
+Message.Create(chat_id: ,user_id: ,body: )
+Message.Create(chat_id: ,user_id: ,body: )
+Message.Create(chat_id: ,user_id: ,body: )
+Message.Create(chat_id: ,user_id: ,body: )
+Message.Create(chat_id: ,user_id: ,body: )
+Message.Create(chat_id: ,user_id: ,body: )
